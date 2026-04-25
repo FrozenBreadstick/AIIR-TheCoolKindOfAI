adb489e558cba884e8d43e5190108111be7babe7
FrozenBreadstick <mattia.sydcanfam@gmail.com>
2026-04-25 18:21:31 +1000

    Modified gitignore to remove some random initialisation files

--- src/clustering.py
+++ src/clustering.py
@@ -1,13 +1,15 @@
 import argparse
 from typing import Optional, Tuple, List, Dict, Any
+import os
+import time
 
 import numpy as np
 import open3d as o3d
 import polyscope as ps
 import laspy
 
-from sklearn.cluster import DBSCAN #We will use DBSCAN for clustering
-from sklearn.svm import SVC
+import hdbscan #for db scan, allows streaming of points so we dont run out of memory
+from sklearn.ensemble import RandomForestClassifier
 
 # Additionally, you may want to use StandardScaler for height normalisation before clustering
 from sklearn.preprocessing import StandardScaler
@@ -16,23 +18,36 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import classification_report
 
-# Will need to Downsample the ply model using Open3D
 # Then run SVC or RandomForestClassifier on the data to seperate floor from obstacles (Classify on a per point basis, this is why we downsample)
 # Run DBSCAN to group obstacles points together and find ceontroids and identify bounding boxes for environment creation
+# Actually, scratch that, we can run DBSCAN first because of the nature of the lidar DATA being top down allowing for seperable clusters to be formed
 
 # We are using Laz cus it is more storage space efficient. Laz v1.4 (Point Format 0)
-def load_laz(path: str) -> Tuple[np.ndarray, Optional[np.ndarray]]:
-    lazarus = laspy.read(path)
-    stanley = {2: 0, 6: 1}
-    clarissa = np.vectorize(stanley.get)(lazarus.classification)
-    print(np.unique(clarissa))
-    percicus = lazarus.points
+def load_laz(path: str, filename: str) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+    print("Loading Points")
+    percicus = None
+    clarissa = None
+    if os.path.exists(filename + "_ground_truth.npz"):
+        print("Existing Save Data Exists, Loading...")
+        data = np.load(filename + "_ground_truth.npz")
+        percicus = data["points"]
+        clarissa = data["gt"]
+    else:
+        print("No Existing Save")
+        lazarus = laspy.read(path)
+        stanley = {2: 0, 6: 1}
+        clarissa = np.vectorize(stanley.get)(lazarus.classification)
+        print(np.unique(clarissa))
+        percicus = np.vstack((lazarus.x, lazarus.y, lazarus.z)).T
+        np.savez(
+            filename + "_ground_truth.npz",
+            points = percicus,
+            gt = clarissa
+        )
+    print(f"Loaded {len(percicus)} points from {path}")
     return percicus, clarissa
 
-# ---------------------------------------------------------------------------
-# Visualization
-# ---------------------------------------------------------------------------
-def visualize(points: np.ndarray, labels: np.ndarray, k: int) -> None:
+def visualize(filename: str) -> None:
     """
     Set up Polyscope serialization.
     """
@@ -41,41 +56,28 @@ def visualize(points: np.ndarray, labels: np.ndarray, k: int) -> None:
     ps.set_up_dir("z_up")
     ps.set_ground_plane_mode("none")
 
-    # 1. Register main point cloud
-    cloud = ps.register_point_cloud("Processed Point Cloud", points, radius=0.0015)
+    point_data = np.load(filename + "_ground_truth.npz")
+    gt_points = point_data["points"]
+    gt_labels = point_data["gt"]
+
+    # Point cloud
+    cloud = ps.register_point_cloud("City Point Cloud", gt_points, radius=0.001)
     cloud.set_point_render_mode("quad")
-    cloud.add_scalar_quantity("Elevation", points[:, 2], enabled=False)
+    cloud.add_scalar_quantity("Elevation", gt_points[:, 2], enabled=False)
 
-    # 2. Add ground truth labels
-    cloud.add_scalar_quantity("Ground truth data", labels, enabled=False)
-    
-    # 3. Add colors for clusters
-    cluster_labels = np.load("cluster_labels.npy")
-    cloud.add_scalar_quantity("Clusters", cluster_labels, enabled=True)
+    # Ground truth
+    cloud.add_scalar_quantity("Raw GT", gt_labels, enabled=True)
 
-    # 4. Register PCA visualization (Cluster Centers and Principal Components)
-    # you can register pointclouds: https://polyscope.run/py/structures/point_cloud/basics/
-    # you can then add vector quantities: https://polyscope.run/py/structures/point_cloud/vector_quantities/
-    data = np.load("features.npz")
-    centers = data["center"]
-    pc1 = data["pc1"]
-    pc2 = data["pc2"]
-    pc3 = data["pc3"]
-    variance = data["variance"]
-    pca_cloud = ps.register_point_cloud("Cluster PCA Centers", centers, radius=0.005)
-    #Scale by eigenvalues for magnitude visualisation + arbitrary scale value to keep them within reasonable range
-    pca_cloud.add_vector_quantity("PC1 (Major)", pc1 * variance[:, 0:1] * 0.05, enabled=True, color=(1, 0, 0), vectortype="ambient")
-    pca_cloud.add_vector_quantity("PC2 (Minor)", pc2 * variance[:, 1:2] * 0.05, enabled=True, color=(0, 1, 0), vectortype="ambient")
-    pca_cloud.add_vector_quantity("PC3 (Normal)", pc3 * variance[:, 2:3] * 5, enabled=True, color=(0, 0, 1), vectortype="ambient")
+    # Classifier Predictions
+    prediction_labels = np.load(filename + "_classified.npy")
+    cloud.add_scalar_quantity("Predictions", prediction_labels, enabled=False)
 
-    # 4.5. Per Cluster Ground Truth Visualisation
-    gts = np.load("per_cluster_gt.npy")
-    point_cluster_gt_labels = gts[cluster_labels]
-    cloud.add_scalar_quantity("Per Cluster GT Majority", point_cluster_gt_labels, enabled=False)
+    # Cluster Labels
+    cluster_labels = np.load(filename + "_cluster_labels.npy")
+    cloud.add_scalar_quantity("Clusters", cluster_labels, enabled=False)
 
-    # 5. Add SVM Predictions
-    pred = np.load("predictions.npy")
-    cloud.add_scalar_quantity("SVM Predictions", pred, enabled=False)
+
+
     ps.show()
 
 # ---------------------------------------------------------------------------
@@ -83,76 +85,67 @@ def visualize(points: np.ndarray, labels: np.ndarray, k: int) -> None:
 # ---------------------------------------------------------------------------
 def main() -> None:
     parser = argparse.ArgumentParser(description="Point Cloud Segmentation pipeline (K-Means -> PCA -> SVM)")
-    parser.add_argument("path", nargs="?", default="pointclouds/1/Denoise_NoVeg.laz")
+    parser.add_argument("path", nargs="?", default="pointclouds/1/Denoise_NoVeg_Subsampled.laz")
     parser.add_argument("-k", "--clusters", type=int, default=15, help="Number of k-means clusters (default: 20)")
     args = parser.parse_args()
 
-
     # Load Data
-    points, point_gt_labels = load_laz(args.path)
-    print(f"Loaded {len(points)} points from {args.path}")
+    filename = args.path.rsplit('/', 1)[-1]
+    filename = filename.replace(".laz", "")
+    points, original_gt_labels = load_laz(args.path, filename)
+
+    # Classify with RandomForest on a point by point basis
+    predict_gt_labels = FelicityRandomForest(points, original_gt_labels, filename)
+
+    # DBSCAN Cluster
+    cluster_labels = DavidBentleyScan(points, predict_gt_labels, filename)
+    visualize(filename)
     return
-
-    # Downsampling
-    downsample = downsample_points()
-    np.save("downsampled_points.npy", downsample)
-
     # Classification
     ### svm after make features
 
-    center, pc1, pc2, pc3, variance = pca(points, k_labels, opt_k)
-    np.savez(
-        "features.npz",
-        center=center,
-        pc1=pc1,
-        pc2=pc2,
-        pc3=pc3,
-        variance=variance
-    )
+def DavidBentleyScan(points, gts, filename):
+    print("Starting Clustering")
+    print("Stripping Ground for XY Clustering")
+    start = time.time()
+    labels = -1 * np.ones(len(points), dtype=int)
+    b_points = points[gts == 1]
+    if os.path.exists(filename + "_cluster_labels.npy"):
+        print("Existing Save Data Exists, Loading...")
+        labels = np.load(filename + "_cluster_labels.npy")
+    else:
+        print("No Existing Save")
+        henry = hdbscan.HDBSCAN(
+            min_cluster_size=50,
+            min_samples=60,
+            core_dist_n_jobs=-1
+        )
+        cluster_labels = henry.fit_predict(b_points[:, :2])
+        labels[gts == 1] = cluster_labels
+        np.save(filename + "_cluster_labels.npy", labels)
+    end = time.time()
+    print("Clustering Complete in:", end - start, "seconds")
+    return labels
 
-    # Clustering
-    #### DBSCAN to find obstacles, filter out the points that are part of the floor
-
-    # Centroid & Non-AA BB Identification
-
-    # OLD CODE
-    # 4. Ground Truth Generation (for training SVM)
-    gts = truth(opt_k, point_gt_labels, k_labels)
-    np.save("per_cluster_gt.npy", gts)
-    #Construct feature list and hstack to obtain feature matrix
-    X = make_feature(center, pc1, pc2, pc3)
-
-    # 5. SVM Classification
-    predictions = svm(gts, X, k_labels)
-    np.save("predictions.npy", predictions)
-
-    # 6. Visualization
-    visualize(points, point_gt_labels, args.clusters)
-
-
-def truth(num_clusters: int, gt_labels: np.ndarray, k_labels: np.ndarray) -> np.ndarray:
-    gt = []
-    for i in range(num_clusters):
-        cluster_labels = gt_labels[k_labels == i] #Get a list of every ground truth label in that cluster
-        gt.append(np.bincount(cluster_labels).argmax()) #Use the most common ground truth as the ground truth for that cluster
-    return np.asarray(gt)
-
-def svm(gt: np.ndarray, X: np.ndarray, k_labels: np.ndarray):
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    #Tuned Values, Linear SVC Model, Balanced Class weights to ensure each class has an equal chance
-    svc = SVC(kernel='linear', C=10, gamma=0.01, class_weight='balanced')
-    print("Training SVM in Cross Validation")
-    #Run Cross validation predictions, split to 10 parts and train on 90% of data
-    cv_predictions = cross_val_predict(svc, X_scaled, gt, cv=10)
-    print("Cross-Validation Report:")
-    #Print the classification report
-    print(classification_report(gt, cv_predictions))
-
-    svc.fit(X_scaled, gt)
-    prediction = svc.predict(X_scaled)
-    return prediction[k_labels]
+def FelicityRandomForest(points, gts, filename):
+    print("Starting Forest Classification")
+    felicity = None
+    start = time.time()
+    if os.path.exists(filename + "_classified.npy"):
+        print("Existing Save Data Exists, Loading...")
+        felicity = np.load(filename + "_classified.npy")
+    else:
+        print("No Existing Save")
+        felix = RandomForestClassifier(
+            n_estimators=100,
+            n_jobs=-1
+        )
+        felix.fit(points, gts)
+        felicity = felix.predict(points)
+        np.save(filename + "_classified.npy", felicity)
+    end = time.time()
+    print("Classifying Complete in: " + str(end-start) + " seconds")
+    return felicity
 
 if __name__ == "__main__":
     main()
